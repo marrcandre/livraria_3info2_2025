bd174d285741b75b0199034d90c7457a5f3f144f
Marco André Mendes <marcoandre@gmail.com>
2025-04-15 14:19:19 -0300

    feat: inclusão do relacionamento n para n entre Livro e Autor.

--- core/models/livro.py
+++ core/models/livro.py
@@ -12,8 +12,7 @@ class Livro(models.Model):
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
-    autor = models.ForeignKey(Autor, on_delete=models.PROTECT, related_name='livros_autor', null=True, blank=True)
-    coautor = models.ForeignKey(Autor, on_delete=models.PROTECT, related_name='livros_coautor', null=True, blank=True)
+    autores = models.ManyToManyField(Autor, related_name="livros", blank=True)
 
     def __str__(self):
         return f'({self.id}) {self.titulo} ({self.quantidade})'
