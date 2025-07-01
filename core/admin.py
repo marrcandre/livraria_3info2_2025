351c8c76ed5690bbc02de571a9f659f0a512b48b
Marco André Mendes <marcoandre@gmail.com>
2025-07-01 14:39:53 -0300

    feat: criação da entidade Compra integrada ao usuário do projeto.

--- core/admin.py
+++ core/admin.py
@@ -6,7 +6,54 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from core.models import Autor, Categoria, Editora, Livro, User
+from core.models import Autor, Categoria, Compra, Editora, Livro, User
+
+
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email')
+    search_fields = ('nome', 'email')
+    list_filter = ('nome',)
+    ordering = ('nome', 'email')
+    list_per_page = 10
+
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('descricao',)
+    search_fields = ('descricao',)
+    list_filter = ('descricao',)
+    ordering = ('descricao',)
+    list_per_page = 10
+
+
+@admin.register(Compra)
+class CompraAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'status')
+    ordering = ('usuario', 'status')
+    list_per_page = 10
+
+
+@admin.register(Editora)
+class EditoraAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'cidade')
+    search_fields = ('nome', 'email', 'cidade')
+    list_filter = ('nome', 'email', 'cidade')
+    ordering = ('nome', 'email', 'cidade')
+    list_per_page = 10
+
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = (
+        'titulo',
+        'categoria',
+        'editora',
+    )
+    search_fields = ('titulo', 'editora__nome', 'categoria__descricao', 'isbn')
+    list_filter = ('editora', 'categoria')
+    ordering = ('categoria', 'editora', 'titulo')
+    list_per_page = 10
 
 
 @admin.register(User)
@@ -60,43 +107,3 @@ class UserAdmin(BaseUserAdmin):
             },
         ),
     )
-
-
-@admin.register(Livro)
-class LivroAdmin(admin.ModelAdmin):
-    list_display = (
-        'titulo',
-        'categoria',
-        'editora',
-    )
-    search_fields = ('titulo', 'editora__nome', 'categoria__descricao', 'isbn')
-    list_filter = ('editora', 'categoria')
-    ordering = ('categoria', 'editora', 'titulo')
-    list_per_page = 10
-
-
-@admin.register(Autor)
-class AutorAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'email')
-    search_fields = ('nome', 'email')
-    list_filter = ('nome',)
-    ordering = ('nome', 'email')
-    list_per_page = 10
-
-
-@admin.register(Categoria)
-class CategoriaAdmin(admin.ModelAdmin):
-    list_display = ('descricao',)
-    search_fields = ('descricao',)
-    list_filter = ('descricao',)
-    ordering = ('descricao',)
-    list_per_page = 10
-
-
-@admin.register(Editora)
-class EditoraAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'email', 'cidade')
-    search_fields = ('nome', 'email', 'cidade')
-    list_filter = ('nome', 'email', 'cidade')
-    ordering = ('nome', 'email', 'cidade')
-    list_per_page = 10
