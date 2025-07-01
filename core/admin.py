74da40c237063804bdd85a7c555dd8b6ae6e1d75
Marco André Mendes <marcoandre@gmail.com>
2025-07-01 15:02:05 -0300

    feat: criação dos itens da compra.

--- core/admin.py
+++ core/admin.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from core.models import Autor, Categoria, Compra, Editora, Livro, User
+from core.models import Autor, Categoria, Compra, Editora, ItensCompra, Livro, User
 
 
 @admin.register(Autor)
@@ -24,6 +24,11 @@ class CategoriaAdmin(admin.ModelAdmin):
     search_fields = ('descricao',)
     list_filter = ('descricao',)
     ordering = ('descricao',)
+    list_per_page = 10
+
+
+@admin.register(ItensCompra)
+class ItensCompraAdmin(admin.ModelAdmin):
     list_per_page = 10
 
 
--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,5 +1,5 @@
 from .categoria import Categoria
-from .compra import Compra
+from .compra import Compra, ItensCompra
 from .editora import Editora
 from .autor import Autor
 from .livro import Livro
