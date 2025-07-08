e08a94792344e7d8e685939923c577c05c89d143
Marco André Mendes <marcoandre@gmail.com>
2025-07-08 14:30:48 -0300

    feat: #23 - criação do endpoint para a listagem básica de compras.

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,5 +1,6 @@
-from .user import UserSerializer
-from .categoria import CategoriaSerializer
-from .editora import EditoraSerializer
 from .autor import AutorSerializer
+from .categoria import CategoriaSerializer
+from .compra import CompraSerializer
+from .editora import EditoraSerializer
 from .livro import LivroListSerializer, LivroRetrieveSerializer , LivroSerializer
+from .user import UserSerializer
