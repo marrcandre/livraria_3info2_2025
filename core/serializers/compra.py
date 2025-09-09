fee74d1d21c421590612bc79fa57e1472b13dcdd
Marco André Mendes <marcoandre@gmail.com>
2025-09-09 14:18:19 -0300

    feat: #33 registrando a data da compra

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,6 +1,7 @@
 from rest_framework.serializers import (
     CharField,
     CurrentUserDefault,
+    DateTimeField,
     HiddenField,
     ModelSerializer,
     SerializerMethodField,
@@ -86,8 +87,9 @@ class ItensCompraSerializer(ModelSerializer):
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source='usuario.email', read_only=True)
     status = CharField(source='get_status_display', read_only=True)
+    data = DateTimeField(read_only=True) # novo campo
     itens = ItensCompraSerializer(many=True, read_only=True)
 
     class Meta:
         model = Compra
-        fields = ('id', 'usuario', 'status', 'total', 'itens')
+        fields = ('id', 'usuario', 'status', 'data', 'total', 'itens')
